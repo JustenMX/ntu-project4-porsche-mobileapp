305c88d60be051b4a7e66ac3b91eef985d6c5243
Justen Manni <me@justenmanni.dev>
2023-11-16 01:45:28 +0800

    Add HomeScreen

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-// import HomeScreen from "./screens/HomeScreen";
-// import SettingScreen from "./screens/SettingScreen";
+import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
-// import ScannerScreen from "./screens/ScannerScreen";
-// import CalendarScreen from "./screens/CalendarScreen";
 
 function App() {
   const Tab = createBottomTabNavigator();
@@ -19,10 +16,7 @@ function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Login" component={LoginScreen} />
-        {/* <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingScreen} /> */}
-        {/* <Tab.Screen name="Calendar" component={CalendarScreen} />
-        <Tab.Screen name="Scanner" component={ScannerScreen} /> */}
+        <Tab.Screen name="Home" component={HomeScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
