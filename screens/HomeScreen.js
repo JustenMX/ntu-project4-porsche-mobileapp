54ebb066a922259a1936f3e94b4f049899a2edf6
Justen Manni <me@justenmanni.dev>
2023-11-16 20:18:47 +0800

    Add LoginScreen with Formik and Yup validation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
-import PorscheOwnerCard from "../components/PorscheOwnerCard";
 
 function HomeScreen() {
   return (
@@ -22,48 +21,52 @@ function HomeScreen() {
 
       <View className="flex-2 items-center justify-between px-1">
         <View className="bg-white rounded-2xl p-4 mt-8 mb-4 shadow-md">
-          <Text className="text-base font-bold">Upcoming Appointments</Text>
+          <Text className="text-base font-bold text-center">
+            Upcoming Appointments
+          </Text>
           {/* Start - Service Card */}
-          <View className="bg-emerald-100 rounded-2xl p-4 mt-4 mb-4 shadow-md">
-            <Text className="text-sm">3 Nov 2023</Text>
+          <View className="bg-blue-100 rounded-2xl p-4 mt-4 mb-4 shadow-md">
+            <Text className="text-sm">20 Nov 2023</Text>
             <Text className="text-xl font-bold">SMZ4044G</Text>
             <Text className="text-base">Porsche Service Centre</Text>
             <Text className="text-base">Tanjong Penjuru</Text>
-            <Text className="text-sm mt-2">32000KM</Text>
+            <Text className="text-sm mt-2">60000KM</Text>
           </View>
           {/* End - Service Card */}
-          <Text className="text-base">
-            Details of your most recent service/repair booking will be shown
+          <Text className="text-sm text-center mt-5">
+            Details of your most recent service / repair booking will be shown
             here
           </Text>
           <View className="border-b border-gray-300 my-4"></View>
-          <Text className="text-base font-bold">Latest Service</Text>
+          <Text className="text-base font-bold text-center">
+            Latest Service
+          </Text>
           {/* Start - Service Card */}
           <View className="bg-emerald-100 rounded-2xl p-4 mt-4 mb-4 shadow-md">
             <Text className="text-sm">3 Nov 2023</Text>
             <Text className="text-xl font-bold">SMZ4044G</Text>
             <Text className="text-base">Porsche Service Centre</Text>
             <Text className="text-base">Tanjong Penjuru</Text>
-            <Text className="text-sm mt-2">32000KM</Text>
+            <Text className="text-sm mt-2">60000KM</Text>
           </View>
           {/* End - Service Card */}
           {/* Start - Service Card */}
           <View className="bg-emerald-100 rounded-2xl p-4 mt-4 mb-4 shadow-md">
-            <Text className="text-sm">3 Nov 2023</Text>
+            <Text className="text-sm">3 Nov 2022</Text>
             <Text className="text-xl font-bold">SMZ4044G</Text>
             <Text className="text-base">Porsche Service Centre</Text>
             <Text className="text-base">Tanjong Penjuru</Text>
-            <Text className="text-sm mt-2">32000KM</Text>
+            <Text className="text-sm mt-2">45000KM</Text>
           </View>
           {/* End - Service Card */}
 
           {/* Start - Service Card */}
           <View className="bg-emerald-100 rounded-2xl p-4 mt-4 mb-4 shadow-md">
-            <Text className="text-sm">3 Nov 2023</Text>
+            <Text className="text-sm">3 Nov 2021</Text>
             <Text className="text-xl font-bold">SMZ4044G</Text>
             <Text className="text-base">Porsche Service Centre</Text>
             <Text className="text-base">Tanjong Penjuru</Text>
-            <Text className="text-sm mt-2">32000KM</Text>
+            <Text className="text-sm mt-2">30000km</Text>
           </View>
           {/* End - Service Card */}
         </View>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,113 +1,242 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
-// import { Button } from "@react-native-material/core";
-// import {
-//   Stack,
-//   TextInput,
-//   IconButton,
-//   Switch,
-// } from "@react-native-material/core";
-// import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import porschelogo from "../assets/porschelogo.png";
 
 function LoginScreen() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [checked, setChecked] = useState(true);
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      optMarketing: false,
+      joinDate: new Date().toISOString(),
+      isSubmitting: true,
+      isValidating: true,
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .matches(/^[A-Za-z\s]+$/, "Only letters and spaces are allowed")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+      password: Yup.string()
+        .min(8, "Password must be at least 8 characters")
+        .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+        .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+        .matches(/\d/, "Password must contain at least one numeric digit")
+        .required("Password is required"),
+      passwordConfirmation: Yup.string()
+        .label("confirm password")
+        .required("Required")
+        .oneOf([Yup.ref("password"), null], "Passwords must match"),
+    }),
+    onSubmit: async (values) => {
+      const currentDate = new Date().toISOString();
+      values.joinDate = currentDate;
 
-  const styles = StyleSheet.create({
-    bgContainer: {
-      backgroundColor: "#fff",
-    },
-    container: {
-      flex: 2,
-      alignItems: "center",
-      justifyContent: "space-evenly",
-    },
-    authContainer: {
-      flex: 1,
-    },
-    logo: {
-      width: 150,
-      height: 200,
-      resizeMode: "contain",
-    },
-    image: {
-      width: 300,
-      height: 200,
-      resizeMode: "contain",
-    },
-    header: {
-      fontSize: 30,
-      textAlign: "center",
-      margin: 10,
-    },
-    text: {
-      textAlign: "center",
-      marginBottom: 50,
-    },
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-    label: {
-      margin: 12,
+      // You can uncomment and add your API call logic here
+      alert(JSON.stringify(values, null, 2));
     },
   });
 
   return (
-    <>
-      <ScrollView style={styles.bgContainer}>
-        {/* logo */}
-        <View style={styles.container}>
-          <Image style={styles.logo} source={porschelogo} />
-        </View>
+    <ScrollView style={{ backgroundColor: "white" }}>
+      <View
+        style={{
+          flex: 2,
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Image
+          style={{
+            width: 80,
+            height: 40,
+            resizeMode: "contain",
+            marginBottom: 2,
+          }}
+          source={porschelogo}
+        />
+      </View>
 
-        {/* Login Credentials */}
-        {/* <View style={styles.authContainer}>
-          <Stack spacing={2} style={{ margin: 16 }}>
-            <TextInput
-              label="email"
-              variant="outlined"
-              trailing={(props) => (
-                <IconButton
-                  icon={(props) => <Icon name="email" {...props} />}
-                  {...props}
-                  color="black"
-                />
-              )}
-            />{" "}
-            <TextInput
-              label="password"
-              secureTextEntry
-              variant="outlined"
-              trailing={(props) => (
-                <IconButton
-                  icon={(props) => <Icon name="lock" {...props} />}
-                  {...props}
-                  color="black"
-                />
-              )}
-            />
-          </Stack>
-          <Stack fill center spacing={4}>
-            <Text>Sports+ Mode</Text>
-            <Switch
-              value={checked}
-              onValueChange={() => setChecked(!checked)}
-            />
-          </Stack>
-          <Button
-            title="Submit"
-            style={{ alignSelf: "center", marginTop: 20 }}
-            onPress={() => alert("🎉🎉🎉")}
-            color="black"
+      <View>
+        <View style={{ marginTop: 8, marginHorizontal: 16 }}>
+          <TextInput
+            style={{
+              marginTop: 8,
+              paddingHorizontal: 12,
+              paddingVertical: 8,
+              borderRadius: 4,
+              borderWidth: 1,
+              borderColor: "#ccc",
+            }}
+            placeholder="Name"
+            onChangeText={formik.handleChange("name")}
+            onBlur={formik.handleBlur("name")}
+            value={formik.values.name}
           />
-        </View> */}
-      </ScrollView>
-    </>
+          {formik.touched.name && formik.errors.name ? (
+            <Text style={{ color: "red", fontSize: 12, fontWeight: "bold" }}>
+              {formik.errors.name}
+            </Text>
+          ) : null}
+
+          <TextInput
+            style={{
+              marginTop: 8,
+              paddingHorizontal: 12,
+              paddingVertical: 8,
+              borderRadius: 4,
+              borderWidth: 1,
+              borderColor: "#ccc",
+            }}
+            placeholder="Email"
+            onChangeText={formik.handleChange("email")}
+            onBlur={formik.handleBlur("email")}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <Text style={{ color: "red", fontSize: 12, fontWeight: "bold" }}>
+              {formik.errors.email}
+            </Text>
+          ) : null}
+
+          <TextInput
+            style={{
+              marginTop: 8,
+              paddingHorizontal: 12,
+              paddingVertical: 8,
+              borderRadius: 4,
+              borderWidth: 1,
+              borderColor: "#ccc",
+            }}
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={formik.handleChange("password")}
+            onBlur={formik.handleBlur("password")}
+            value={formik.values.password}
+          />
+          {formik.touched.password && formik.errors.password ? (
+            <Text style={{ color: "red", fontSize: 12, fontWeight: "bold" }}>
+              {formik.errors.password}
+            </Text>
+          ) : null}
+
+          <TextInput
+            style={{
+              marginTop: 8,
+              paddingHorizontal: 12,
+              paddingVertical: 8,
+              borderRadius: 4,
+              borderWidth: 1,
+              borderColor: "#ccc",
+            }}
+            placeholder="Password Confirmation"
+            secureTextEntry
+            onChangeText={formik.handleChange("passwordConfirmation")}
+            onBlur={formik.handleBlur("passwordConfirmation")}
+            value={formik.values.passwordConfirmation}
+          />
+          {formik.touched.passwordConfirmation &&
+          formik.errors.passwordConfirmation ? (
+            <Text style={{ color: "red", fontSize: 12, fontWeight: "bold" }}>
+              {formik.errors.passwordConfirmation}
+            </Text>
+          ) : null}
+
+          <View
+            style={{ flexDirection: "row", alignItems: "center", marginTop: 8 }}
+          >
+            <TouchableOpacity
+              style={{
+                height: 20,
+                width: 20,
+                borderRadius: 4,
+                borderWidth: 1,
+                borderColor: "#ccc",
+              }}
+              onPress={() =>
+                formik.setFieldValue(
+                  "optMarketing",
+                  !formik.values.optMarketing
+                )
+              }
+            >
+              {formik.values.optMarketing && (
+                <View
+                  style={{
+                    flex: 1,
+                    backgroundColor: "#4285F4",
+                    borderRadius: 4,
+                  }}
+                />
+              )}
+            </TouchableOpacity>
+            <Text style={{ marginLeft: 8, fontSize: 12, color: "#333" }}>
+              I want to receive emails about events, product updates and company
+              announcements.
+            </Text>
+          </View>
+
+          <Text style={{ marginTop: 8, fontSize: 12, color: "#888" }}>
+            By creating an account, you agree to our
+            <Text style={{ textDecorationLine: "underline", color: "#4285F4" }}>
+              {/* Add link to terms */}
+            </Text>
+            and
+            <Text
+              style={{
+                marginLeft: 4,
+                textDecorationLine: "underline",
+                color: "#4285F4",
+              }}
+            >
+              {/* Add link to privacy */}
+            </Text>
+            .
+          </Text>
+
+          <View
+            style={{ flexDirection: "row", alignItems: "center", marginTop: 8 }}
+          >
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                backgroundColor: "#4285F4",
+                borderRadius: 4,
+                paddingVertical: 10,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={formik.handleSubmit}
+            >
+              <Text
+                style={{ color: "white", fontSize: 14, fontWeight: "bold" }}
+              >
+                Create an account
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <Text style={{ marginTop: 4, fontSize: 12, color: "#888" }}>
+            Already have an account?
+            <Text style={{ textDecorationLine: "underline", color: "#4285F4" }}>
+              {/* Add link to sign in */}
+            </Text>
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
