6f066f224ec356b5cb5892fb7ef5e9110b513918
Justen <me@justenmanni.dev>
2023-11-17 22:33:27 +0800

    Add LogoutScreen

---
+++ screens/LogOutScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View, ScrollView } from "react-native";
+
+function LogoutScreen() {
+  return (
+    <ScrollView className="bg-white">
+      <View className="flex-2 items-center justify-between">
+        <Text>Hello</Text>
+      </View>
+    </ScrollView>
+  );
+}
